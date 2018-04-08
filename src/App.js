f477b6e67b44b6ff3fc1a67cb5374a36a3878a35
farkhad <farkhadcity@gmail.com>
2018-04-08 23:32:42 +0300

    add webpack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.scss";
+//import MainScreen from "./containers/MainScreen";
+
+const test = [4, 4, 4, 45, 45, 56];
+
+class App extends Component {
+  render() {
+    return <h1>test</h1>;
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+//import rootReducer from "./store/reducers";
+
+//const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
